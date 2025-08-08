2f03f697db09c0152d9a7d60d0c596e5dc274a87
Miraubolant <143420936+Miraubolant@users.noreply.github.com>
2025-08-08 19:44:12 +0200

    Réorganiser l'ordre des sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Layout from './components/layout/Layout';
 import Hero from './components/sections/Hero';
-import About from './components/sections/About';
-import InteractiveDemo from './components/sections/InteractiveDemo';
 import Process from './components/sections/Process';
+import InteractiveDemo from './components/sections/InteractiveDemo';
+import About from './components/sections/About';
 import Portfolio from './components/sections/Portfolio';
 import Contact from './components/sections/Contact';
 import './styles/globals.css';
@@ -14,9 +14,9 @@ function App() {
     <Layout>
       <PerformanceOptimizer />
       <Hero />
-      <About />
-      <InteractiveDemo />
       <Process />
+      <InteractiveDemo />
+      <About />
       <Portfolio />
       <Contact />
     </Layout>
