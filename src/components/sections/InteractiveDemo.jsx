b306cc94302cec42290df449c0c8f86d1392805a
Miraubolant <143420936+Miraubolant@users.noreply.github.com>
2025-08-10 18:53:17 +0200

    Supprimer les boutons de navigation du navigateur

--- src/components/sections/InteractiveDemo.jsx
+++ src/components/sections/InteractiveDemo.jsx
@@ -433,23 +433,6 @@ const DesktopPreview = ({ demoState, activeControl }) => {
           <div className="w-3 h-3 bg-green-500 rounded-full shadow-md hover:bg-green-400 transition-all cursor-pointer hover:scale-110"></div>
         </div>
         <div className="flex items-center space-x-2 flex-1">
-          <div className="flex space-x-1">
-            <div className="w-4 h-4 bg-gray-600 rounded hover:bg-gray-500 transition-colors cursor-pointer flex items-center justify-center">
-              <svg className="w-2 h-2 text-gray-300" fill="currentColor" viewBox="0 0 20 20">
-                <path fillRule="evenodd" d="M7.707 14.707a1 1 0 01-1.414 0L2.586 11l3.707-3.707a1 1 0 011.414 1.414L5.414 11l2.293 2.293a1 1 0 010 1.414z" clipRule="evenodd" />
-              </svg>
-            </div>
-            <div className="w-4 h-4 bg-gray-600 rounded hover:bg-gray-500 transition-colors cursor-pointer flex items-center justify-center">
-              <svg className="w-2 h-2 text-gray-300" fill="currentColor" viewBox="0 0 20 20">
-                <path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4.414 4.414-4.414 4.414a1 1 0 01-1.414-1.414L14.586 11l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd" />
-              </svg>
-            </div>
-            <div className="w-4 h-4 bg-gray-600 rounded hover:bg-gray-500 transition-colors cursor-pointer flex items-center justify-center">
-              <svg className="w-2 h-2 text-gray-300" fill="currentColor" viewBox="0 0 20 20">
-                <path fillRule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clipRule="evenodd" />
-              </svg>
-            </div>
-          </div>
           <div className="flex-1 bg-gray-700 rounded-lg px-4 py-2 text-sm text-gray-300 font-mono border border-gray-600 flex items-center">
             <svg className="w-3 h-3 text-green-400 mr-2" fill="currentColor" viewBox="0 0 20 20">
               <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
